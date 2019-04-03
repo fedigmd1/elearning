16b038006c875195f9426462898fcb3444385c86
fedi gammoudi <gammoudifedi@gmail.com>
2019-04-03 23:02:36 +0100

    Fix profil page

--- imports/ui/pages/profil/profil.jsx
+++ imports/ui/pages/profil/profil.jsx
@@ -1,15 +1,28 @@
 import React, {Component}from 'react'
 import ReactDOM from 'react-dom'
+import { Meteor } from 'meteor/meteor';
+import {withTracker} from 'meteor/react-meteor-data'
 
 
-export default class profil extends Component {
+class profil extends Component {
 
 
   render () {
     return (
       <div>
-        <h1>hhhhh</h1>
+        <h1>Test</h1>
+        
+        <h1>It Works!!</h1>
       </div>
     )
   }
 }
+
+export default withTracker(() => {
+  let users= Meteor.users
+  console.log(users);
+  return {
+    currentUser: Meteor.user(),
+    users,
+  };
+}) (profil);
