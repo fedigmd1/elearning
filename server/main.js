6b7e19ebb0561b9f3a38b28b552b1af5c7338cb3
fedi gammoudi <gammoudifedi@gmail.com>
2019-05-09 14:55:22 +0100

    server

--- server/main.js
+++ server/main.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/courses.jsx';
-import '../imports/api/users.jsx';
 import '../imports/api/elements.jsx';
+import '../imports/api/explications';
 import '../imports/api/forums.jsx';
 import '../imports/api/reclamations';
+import '../imports/api/users.jsx';
 
 
 Meteor.startup(() => {
